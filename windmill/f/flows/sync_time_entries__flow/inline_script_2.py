50cbfd9cabbb85295fe092b0c7a454ac675ec6f5
Yevhen Chubchyk <eugen.chubchik@gmail.com>
2026-01-09 17:38:18 +0100

    feat: Implement script to fetch detailed Openproject entity data

---
+++ windmill/f/flows/sync_time_entries__flow/inline_script_2.py
@@ -0,0 +1,27 @@
+import wmill
+import requests
+
+
+def main(x: str):
+    """
+    Executes a GET request to a specified API endpoint and retrieves entity data.
+
+    The function retrieves authentication details and API URL from a secure source,
+    performs a GET request using the provided input as an endpoint, and extracts
+    the 'entity' data from the response.
+
+    Parameters:
+    x (str): The endpoint to append to the API URL.
+
+    Returns:
+    dict: A dictionary containing the 'entity' data retrieved from the API response.
+    """
+    authorization_hash = wmill.get_variable("u/admin/op_authorization_hash")
+    op_api_url = wmill.get_variable("u/admin/op_api_url")
+    r = requests.get(
+        f"{op_api_url}{x}",
+        headers={
+            "Authorization": f"Basic {authorization_hash}"
+        },
+    )
+    return r.json().get("_embedded", {}).get("entity", {})
